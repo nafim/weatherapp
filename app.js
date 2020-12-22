6562ff1864024d931f99839f2060ada44f0f4f60
Nafim Rahman <snrahman2010@gmail.com>
2020-12-22 17:48:26 -0500

    created cookie support

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const express = require('express');
 const app = express();
+var axios = require("axios");
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
+
+require('dotenv').config();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.set('view engine', 'pug')
+app.get('/', (req, res) => {  
+  const zip = req.query.zipcode || req.cookies.zipcode || "02446" ;
+  const queryParams = new URLSearchParams({
+    zip,
+    appid: process.env.OWM_API_KEY
+});
+
+  axios('http://api.openweathermap.org/data/2.5/weather?' + queryParams)
+  .then(response => {
+    const conditions = response.data.weather[0].main;
+    const city = response.data.name;
+    const imgURL = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@4x.png`;
+    res.cookie("zipcode", zip);
+    res.render('weather', {conditions, city, imgURL});
+  })
+  .catch(error => {
+      console.log(error);
+  });
 });
 
 app.listen(port, () => {
