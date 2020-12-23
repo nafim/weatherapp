621773a63e9d9ad13e820c83a1fadad365121b8d
Nafim Rahman <snrahman2010@gmail.com>
2020-12-23 13:32:39 -0500

    Added weather info

--- app.js
+++ app.js
@@ -19,9 +19,13 @@ app.get('/', (req, res) => {
   .then(response => {
     const conditions = response.data.weather[0].main;
     const city = response.data.name;
+    const temperature = (response.data.main.temp - 273.15) * 9/5 + 32;
+    const humidity = response.data.main.humidity;
+    const speed = response.data.wind.speed;
+    const degree = response.data.wind.deg;
     const imgURL = `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@4x.png`;
     res.cookie("zipcode", zip);
-    res.render('weather', {conditions, city, imgURL});
+    res.render('weather', {conditions, city, temperature, humidity, speed, degree, imgURL});
   })
   .catch(error => {
       console.log(error);
